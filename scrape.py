5b4db804a18de7b6e3fdca4b3dd02e7d461a557c
BrandonOng07 <brandonong07@gmail.com>
2026-03-30 22:54:22 -0700

    attempting to grab updated coursework

--- scrape.py
+++ scrape.py
@@ -13,13 +13,28 @@ soup.find_all("div", class_="courseblock")
 # def __init__(self, name, code, units, description):
 blocks = soup.find_all("div", class_="courseblock")
 for block in blocks:
-    courseCode = block.find("span", class_="detail-code").text
-    courseName = block.find("span", class_="detail-title").text
-    courseUnits = block.find("span", class_="detail-hours_html").text
     courseDesc = block.find("p", class_="courseblockextra").text
+
+    # Checking for oudated courses
+    if "This version has ended;" in courseDesc:
+        subBlock = block.find("div", class_="courseblockextra")
+        lis = subBlock.find_all("li")
+        for li in lis:
+            label = li.find("span", class_="label")
+            if label:
+                label_text = label.get_text(strip=True)
+            full_text = li.get_text(strip=True)
+            result = full_text.replace(label_text, "").strip()
+            print(result)
+    else:
+        courseCode = block.find("span", class_="detail-code").text
+        courseName = block.find("span", class_="detail-title").text
+        courseUnits = block.find("span", class_="detail-hours_html").text
+
 
     courseCode = courseCode.replace(" ", "")
     courseName = courseName.replace("—", "").strip()
 
     courseObj = course.Course(courseName, courseCode, courseUnits, courseDesc)
     course.courses[courseCode] = courseObj
+
